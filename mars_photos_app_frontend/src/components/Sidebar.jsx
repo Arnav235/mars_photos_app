3c3bfafda30bb10d50b7182a3203c1799b4f8ccb
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-07-06 14:29:47 -0700

    Changed data state via the sidebar

--- mars_photos_app_frontend/src/components/Sidebar.jsx
+++ mars_photos_app_frontend/src/components/Sidebar.jsx
@@ -1,13 +1,23 @@
 import React from "react";
 
-function Sidebar() {
+function Sidebar({ updateCategory }) {
   return (
     <div className="flex flex-col mt-10 text-lg">
-      <p className="mt-10 cursor-pointer">Chemistry and Camera Complex</p>
-      <p className="mt-10 cursor-pointer">Front Hazard Avoidance Camera</p>
-      <p className="mt-10 cursor-pointer">Rear Hazard Avoidance Camera</p>
-      <p className="mt-10 cursor-pointer">Mast Camera</p>
-      <p className="mt-10 cursor-pointer">Navigation Camera</p>
+      <p className="mt-10 cursor-pointer" onClick={() => updateCategory(0)}>
+        Chemistry and Camera Complex
+      </p>
+      <p className="mt-10 cursor-pointer" onClick={() => updateCategory(1)}>
+        Front Hazard Avoidance Camera
+      </p>
+      <p className="mt-10 cursor-pointer" onClick={() => updateCategory(2)}>
+        Rear Hazard Avoidance Camera
+      </p>
+      <p className="mt-10 cursor-pointer" onClick={() => updateCategory(3)}>
+        Mast Camera
+      </p>
+      <p className="mt-10 cursor-pointer" onClick={() => updateCategory(4)}>
+        Navigation Camera
+      </p>
     </div>
   );
 }
