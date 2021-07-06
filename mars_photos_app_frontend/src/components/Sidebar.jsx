9829faf09aef3b1ed25429ccbd4a60a92e93911f
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-07-06 13:08:58 -0700

    Seperated code into navbar, sidebar, and footer components

---
+++ mars_photos_app_frontend/src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="flex flex-col mt-10 text-lg">
+      <p className="mt-10">Chemistry and Camera Complex</p>
+      <p className="mt-10">Front Hazard Avoidance Camera</p>
+      <p className="mt-10">Rear Hazard Avoidance Camera</p>
+      <p className="mt-10">Mast Camera</p>
+      <p className="mt-10">Navigation Camera</p>
+    </div>
+  );
+}
+
+export default Sidebar;
--- mars_photos_app_frontend/src/screens/Home.jsx
+++ mars_photos_app_frontend/src/screens/Home.jsx
@@ -1,28 +1,15 @@
 import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+import Sidebar from "../components/Sidebar";
 
 function Home() {
-  var today = new Date();
-  var day = String(today.getDate()).padStart(2, "0");
-  var month = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
-  var year = today.getFullYear();
-
   return (
     <div className="bg-yellow-100 min-h-screen flex flex-col font-mono">
       <div className="flex flex-col p-10">
-        <div className="flex justify-between">
-          <h1 className="text-2xl font-bold">Mars Photo App</h1>
-          <h1 className="text-2xl font-bold">
-            Today's Date: {`${month}/${day}/${year}`}
-          </h1>
-        </div>
+        <Navbar />
         <div className="flex">
-          <div className="flex flex-col mt-10 text-lg">
-            <p className="mt-10">Chemistry and Camera Complex</p>
-            <p className="mt-10">Front Hazard Avoidance Camera</p>
-            <p className="mt-10">Rear Hazard Avoidance Camera</p>
-            <p className="mt-10">Mast Camera</p>
-            <p className="mt-10">Navigation Camera</p>
-          </div>
+          <Sidebar />
           <div className="w-full ml-10 pt-5 flex items-center justify-center flex-wrap">
             {new Array(20).fill(1).map((item, index) => (
               <img
@@ -35,10 +22,7 @@ function Home() {
           </div>
         </div>
       </div>
-      <div className="flex p-10 justify-between fixed left-0 bottom-0 w-screen h-10">
-        <p className="text-xl font-bold">Home</p>
-        <p className="text-xl font-bold">Refresh</p>
-      </div>
+      <Footer />
     </div>
   );
 }
