826f9707a57833e60869d7b0e3ed015874daf1ab
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-07-06 11:30:26 -0700

    Added content for navbar and footer

--- mars_photos_app_frontend/src/screens/Home.jsx
+++ mars_photos_app_frontend/src/screens/Home.jsx
@@ -3,7 +3,20 @@ import React from "react";
 function Home() {
   return (
     <div>
-      <p>Home</p>
+      <div>
+        <h1>Mars Photo App</h1>
+        <div>
+          <p>Chemistry and Camera Complex</p>
+          <p>Front Hazard Avoidance Camera</p>
+          <p>Rear Hazard Avoidance Camera</p>
+          <p>Mast Camera</p>
+          <p>Navigation Camera</p>
+        </div>
+      </div>
+      <div>
+        <p>Home</p>
+        <p>Refresh</p>
+      </div>
     </div>
   );
 }
