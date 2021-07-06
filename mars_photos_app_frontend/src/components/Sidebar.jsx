c043726517f5f45dc2171b237df10b6cda5bdf17
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-07-06 14:20:49 -0700

    Added parent state and data in home component

--- mars_photos_app_frontend/src/components/Sidebar.jsx
+++ mars_photos_app_frontend/src/components/Sidebar.jsx
@@ -3,11 +3,11 @@ import React from "react";
 function Sidebar() {
   return (
     <div className="flex flex-col mt-10 text-lg">
-      <p className="mt-10">Chemistry and Camera Complex</p>
-      <p className="mt-10">Front Hazard Avoidance Camera</p>
-      <p className="mt-10">Rear Hazard Avoidance Camera</p>
-      <p className="mt-10">Mast Camera</p>
-      <p className="mt-10">Navigation Camera</p>
+      <p className="mt-10 cursor-pointer">Chemistry and Camera Complex</p>
+      <p className="mt-10 cursor-pointer">Front Hazard Avoidance Camera</p>
+      <p className="mt-10 cursor-pointer">Rear Hazard Avoidance Camera</p>
+      <p className="mt-10 cursor-pointer">Mast Camera</p>
+      <p className="mt-10 cursor-pointer">Navigation Camera</p>
     </div>
   );
 }
