e93cb9820fc572ca1bda8c5af6fa2081c182e75d
Mihir-Achyuta <mihirachyuta@gmail.com>
2021-07-06 14:41:02 -0700

    Implemented home and refresh functions for the footer

--- mars_photos_app_frontend/src/components/Footer.jsx
+++ mars_photos_app_frontend/src/components/Footer.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
-function Footer() {
+function Footer({ updateCategory }) {
   return (
     <div className="flex p-10 justify-between fixed left-0 bottom-0 w-screen h-10">
-      <p className="text-xl font-bold">Home</p>
-      <p className="text-xl font-bold">Refresh</p>
+      <p
+        className="text-xl font-bold cursor-pointer"
+        onClick={() => updateCategory(0)}
+      >
+        Home
+      </p>
+      <p
+        className="text-xl font-bold cursor-pointer"
+        onClick={() => window.location.reload()}
+      >
+        Refresh
+      </p>
     </div>
   );
 }
--- mars_photos_app_frontend/src/screens/Home.jsx
+++ mars_photos_app_frontend/src/screens/Home.jsx
@@ -24,7 +24,7 @@ function Home() {
           />
         </div>
       </div>
-      <Footer />
+      <Footer updateCategory={updateCategory} />
     </div>
   );
 }
